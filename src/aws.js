fc1e7f7c598aee1fea4cff8d8acdcdf9951055b5
lcardno10 <88373479+lcardno10@users.noreply.github.com>
2024-08-29 10:14:30 +0100

    Assume ubuntu in the runner

--- src/aws.js
+++ src/aws.js
@@ -19,15 +19,14 @@ function buildUserDataScript(githubRegistrationToken, label) {
   } else {
     return [
       '#!/bin/bash',
-      'mkdir actions-runner && cd actions-runner',
-      `echo "${config.input.preRunnerScript}" > pre-runner-script.sh`,
+      'sudo -u ubuntu mkdir /home/ubuntu/actions-runner && cd /home/ubuntu/actions-runner',
+      `sudo -u ubuntu echo "${config.input.preRunnerScript}" > pre-runner-script.sh`,
       'source pre-runner-script.sh',
-      'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
-      'curl -O -L https://github.com/actions/runner/releases/download/v2.313.0/actions-runner-linux-${RUNNER_ARCH}-2.313.0.tar.gz',
-      'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.313.0.tar.gz',
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-      './run.sh',
+      'sudo -u ubuntu case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
+      'sudo -u ubuntu curl -O -L https://github.com/actions/runner/releases/download/v2.313.0/actions-runner-linux-${RUNNER_ARCH}-2.313.0.tar.gz',
+      'sudo -u ubuntu tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.313.0.tar.gz',
+      `sudo -u ubuntu ./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+      'sudo -u ubuntu ./run.sh',
     ];
   }
 }
