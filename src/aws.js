0edd0356f0714590eed066433f9080582d7b69cf
peter-woodcock <pwoodcock@no10.gov.uk>
2025-08-05 11:05:34 +0100

    Add InstanceInitiatedShutdownBehavioraccount to ec2 startup

--- src/aws.js
+++ src/aws.js
@@ -61,6 +61,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
+    InstanceInitiatedShutdownBehavior: 'terminate',
     MetadataOptions: {
       HttpTokens: 'required',
       HttpEndpoint: 'enabled',
